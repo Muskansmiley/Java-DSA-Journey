b61ba05a64f0b3cd4f0e1a8b2a04674e91212eb1
MUSKAN <muskan.smiley04@gmail.com>
2026-04-03 21:49:52 +0530

    Day 1 Java basics programs

--- Day01_Basics/HelloWorld.java
+++ Day01_Basics/HelloWorld.java
@@ -1,5 +1,9 @@
-public class HelloWorld{
-    public static void main(String[]args){
+public class HelloWorld {
+
+    public static void main(String[] args){
+
         System.out.println("Start of my SDE Journey");
+
     }
+
 }
