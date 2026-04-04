726542e1e28eeff1c30fc03efc0e6c7481c8802f
MUSKAN <muskan.smiley04@gmail.com>
2026-04-04 16:28:30 +0530

    PrintLines

---
+++ Day01_Basics/PrintLines.java
@@ -0,0 +1,8 @@
+public class PrintLines {
+    public static void main(String[]args){
+        System.out.println("Muskan");
+        System.out.println("Future SDE");
+        System.out.println("Day 1 of consistency");
+    }
+    
+}
