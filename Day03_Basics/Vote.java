c68872fd2cd82ff9685d8156144b61eaeb7ae6f4
MUSKAN <muskan.smiley04@gmail.com>
2026-04-22 15:04:19 +0530

    New Change

--- Day03_Basics/Vote.java
+++ Day03_Basics/Vote.java
@@ -8,7 +8,7 @@ public class Vote {
     System.out.print("Enter Age: ");
     int age=sc.nextInt();
 
-    if(age>18){
+    if(age>=18){
         System.out.println("Eligible to vote");
     }
     else{
