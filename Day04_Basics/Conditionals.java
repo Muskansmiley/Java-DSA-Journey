32306cd5d70456eceb62236f823473f8ac9471c0
MUSKAN <muskan.smiley04@gmail.com>
2026-04-22 15:29:35 +0530

    New Change

---
+++ Day04_Basics/Conditionals.java
@@ -0,0 +1,16 @@
+//Conditional Statements- if, else, else if, switch, break
+import java.util.*;
+public class Conditionals {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age: ");
+        int age=sc.nextInt();
+
+        if(age>18){
+            System.out.println("Adult");
+        }else{
+            System.out.println("Not Adult");
+        }
+
+    }
+}
