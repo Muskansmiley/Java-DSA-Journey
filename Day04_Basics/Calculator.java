3c12a02e49aa707771ccf3c95b37ac88e600f8ba
MUSKAN <muskan.smiley04@gmail.com>
2026-04-23 21:18:58 +0530

    New Change

---
+++ Day04_Basics/Calculator.java
@@ -0,0 +1,42 @@
+// Make a Calculator. Take 2 numbers (a & b) from the user and an operation as follows : 
+// 1 : + (Addition) a + b
+// 2 : - (Subtraction) a - b
+// 3 : * (Multiplication) a * b
+// 4 : / (Division) a / b
+// 5 : % (Modulo or remainder) a % b
+// Calculate the result according to the operation given and display it to the user.
+
+import java.util.*;
+public class Calculator{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        int a=sc.nextInt();
+        System.out.println("Enter num2: ");
+        int b=sc.nextInt();
+        System.out.println("Enter operation number: ");
+        int operator=sc.nextInt();
+
+        switch(operator){
+            case 1: System.out.println(a+b);
+            break;
+            case 2: System.out.println(a-b);
+            break;
+            case 3: System.out.println(a*b);
+            break;
+            case 4: 
+            if(b!=0){
+                System.out.println((double)a/b);
+            }else{
+                System.out.println("Cannot divide by zero");
+            }
+            
+            break;
+            case 5: System.out.println(a%b);
+            break;
+            default: System.out.println("Invalid");
+        }
+        sc.close();
+
+    }
+}
