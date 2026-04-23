4aa81741fafd72be13a9331c6864a0bad2f754f3
MUSKAN <muskan.smiley04@gmail.com>
2026-04-23 18:03:56 +0530

    New Change

---
+++ Day04_Basics/Button.java
@@ -0,0 +1,35 @@
+// Q -> print greeting
+// Button 1 -> Hello
+// Button 2 -> Namaste
+// Button 3 -> Bonjour
+
+import java.util.*;
+public class Button {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Button Number: ");
+        int button=sc.nextInt();
+
+        // if(button==1){
+        //     System.out.println("Hello");
+        // } else if(button==2){
+        //     System.out.println("Namaste");
+        // } else if(button==3){
+        //     System.out.println("Bonjour");
+        // }else{
+        //     System.out.println("Invalid button");
+        // }
+
+        //using switch
+
+        switch(button){
+            case 1: System.out.println("Hello");
+            break;
+            case 2: System.out.println("Namaste");
+            break;
+            case 3: System.out.println("Bonjour");
+            break;
+            default: System.out.println("Invalid Button");
+        }
+    }
+}
