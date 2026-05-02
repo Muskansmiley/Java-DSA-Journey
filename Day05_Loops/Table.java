e0c96d9b6e11e686c4ed86e8a3583e0435cd522c
MUSKAN <muskan.smiley04@gmail.com>
2026-05-02 11:53:20 +0530

    New Change

--- Day05_Loops/Table.java
+++ Day05_Loops/Table.java
@@ -1,18 +1,14 @@
-//  QS-> Print the sum of first n Natural Numbers:
-
 import java.util.*;
-public class SumOfNatural {
+public class Table {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter n: ");
         int n=sc.nextInt();
 
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            sum=sum+i;
+        for(int i=1;i<11;i++){
+            System.out.println(n*i);
+            
         }
-        System.out.println(sum);
         sc.close();
     }
-    
 }
