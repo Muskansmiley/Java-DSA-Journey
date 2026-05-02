759dc11658b6a0783dcc0497e502b7270d65debc
MUSKAN <muskan.smiley04@gmail.com>
2026-05-02 12:31:55 +0530

    New Change

---
+++ Day05_Loops/Infinite.java
@@ -0,0 +1,13 @@
+//Qs-> Run
+// for(; ;) {
+//             System.out.println("Apna College");
+//         }
+// loop on your system and analyze what happens. Try to think of the reason for the output produced.
+
+public class Infinite {
+    public static void main(String[] args) {
+        for( ; ;){
+            System.out.println("Apna College");
+        }
+    }
+}
