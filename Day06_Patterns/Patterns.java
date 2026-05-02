1d16b68e956c501e647cc568d3f6d5d6bf22a0bc
MUSKAN <muskan.smiley04@gmail.com>
2026-05-02 21:37:45 +0530

    New Change

---
+++ Day06_Patterns/Patterns.java
@@ -0,0 +1,17 @@
+// Print the Pattern:
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+// very basic pattern with 1 loop 
+
+import java.util.*;
+public class Patterns {
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=4; i++){
+            System.out.println("* * * *");
+        }
+    }
+}
