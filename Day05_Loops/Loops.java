36ce036bbe1e33f0f9f7907f837be0ff7410e783
MUSKAN <muskan.smiley04@gmail.com>
2026-05-02 11:37:59 +0530

    New Change

---
+++ Day05_Loops/Loops.java
@@ -0,0 +1,37 @@
+// for(initialisation; condition; updation){
+//   //do something }
+
+// while(condition){
+//      //do something }
+public class Loops {
+    public static void main(String[] args) {
+
+        //for loop to print 0-10
+
+        // for(int i=0; i<11; i++){
+        //     System.out.print(i+" ");
+        // }
+        // System.out.println();
+        
+        // for loop to print 11-1
+
+        // for(int j=11; j>0; j--){
+        //     System.out.print(j+ " ");
+        // }
+
+        //while loop
+        // int i=0;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        //do-while loop 
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+
+        }while(i<11);
+    }
+}
