8a6c2dbe3f2a5c06dd7b1b5dd59c172321595ffe
MUSKAN <muskan.smiley04@gmail.com>
2026-05-07 22:32:14 +0530

    New Change

---
+++ Day07_AdvancedPatterns/SolidRhombus.java
@@ -0,0 +1,26 @@
+//Qs-> Print the Pattern
+//         * * * * *
+//       * * * * *
+//     * * * * *
+//   * * * * *
+// * * * * *
+
+public class SolidRhombus {
+    public static void main(String[] args) {
+        int n=5;
+
+        //outer loop
+        for(int i=1; i<=n; i++){
+            //spaces
+            int spaces=n-i;
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
