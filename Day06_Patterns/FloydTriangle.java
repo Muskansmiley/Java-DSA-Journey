9a45cffa433ee5920af90c220d7479ddc3c8cdd0
MUSKAN <muskan.smiley04@gmail.com>
2026-05-07 11:08:48 +0530

    New Change

---
+++ Day06_Patterns/FloydTriangle.java
@@ -0,0 +1,29 @@
+// Qs-> Print the pattern:
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10 
+// 11 12 13 14 15
+
+// incrementing number in each row
+
+import java.util.*;
+public class FloydTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n=sc.nextInt();
+        int number =1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(number+" ");
+                number++; //number=number+1;
+            }
+            System.out.println();
+        }
+        sc.close();
+
+        
+    }
+}
