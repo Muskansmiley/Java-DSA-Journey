a0180260e402434fb230056f061a3d357bbe5f76
MUSKAN <muskan.smiley04@gmail.com>
2026-05-07 10:34:46 +0530

    New Change

---
+++ Day06_Patterns/HalfPyramidNumbers.java
@@ -0,0 +1,28 @@
+//Qs-> Print the pattern
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// basically printing numbers acc to row number
+
+import java.util.*;
+public class HalfPyramidNumbers {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the number of rows: ");
+    int n=sc.nextInt();
+
+    //outer loop
+    for(int i=1; i<=n; i++){
+      //inner loop
+      for(int j=1; j<=i; j++){
+         System.out.print(j+" ");
+      }
+      System.out.println();
+    }
+    sc.close();
+
+   } 
+}
