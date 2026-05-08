a8f9f3bb7d49cc673697f40d1e7896fad100eacd
MUSKAN <muskan.smiley04@gmail.com>
2026-05-08 22:11:50 +0530

    New Change

---
+++ Day08_Functions/InfiniteLoop.java
@@ -0,0 +1,9 @@
+//Qs-> Write an infinite loop using do while condition.
+public class InfiniteLoop {
+    public static void main(String[] args) {
+        do{
+            System.out.println("Infinite Loop");
+
+        }while(true);
+    }
+}
