42cbb37463f4eaafe95d5e4218e1b59a8fd19d9b
MUSKAN <muskan.smiley04@gmail.com>
2026-05-08 17:36:55 +0530

    New Change

---
+++ Day08_Functions/Factorial.java
@@ -0,0 +1,31 @@
+//Find the factorial of number 
+
+import java.util.*;
+public class Factorial {
+    public static void printFactorial(int n){
+
+        if(n<0){
+            System.out.println("Invalid Number");
+        }
+
+        int factorial=1;
+
+        for(int i=n; i>=1; i--){
+            factorial=factorial*i;
+
+        }
+        System.out.println(factorial);
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n=sc.nextInt();
+
+        printFactorial(n);
+
+        sc.close();
+
+
+    }
+}
